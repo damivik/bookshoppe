63d1a0d32b440658465624f059e412820442d48f
Dami Oyediran <oyediran.viktor@gmail.com>
2022-05-01 19:16:25 +0100

    Initial commit

---
+++ src/main/java/com/github/damivik/bookshoppe/BookshoppeApplication.java
@@ -0,0 +1,13 @@
+package com.github.damivik.bookshoppe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshoppeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookshoppeApplication.class, args);
+	}
+
+}
